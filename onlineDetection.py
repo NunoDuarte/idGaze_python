de5876de923c6429c02fc94e8543cf3c900a2ae9
NunoDuarte <nunoduarte.cc.fduarte@gmail.com>
2019-12-18 20:49:59 +0100

    update import libs from tensorflow models

--- onlineDetection.py
+++ onlineDetection.py
@@ -18,8 +18,10 @@ import imutils
 # import for face detection
 import tensorflow as tf
 import os
-from utils import label_map_util
-from utils import visualization_utils as vis_util
+#from utils import label_map_util
+from object_detection.utils import label_map_util
+from object_detection.utils import visualization_utils as vis_util
+#from utils import visualization_utils as vis_util
 
 """
 Receive world camera data from Pupil using ZMQ.
